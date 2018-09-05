1c2f91de145b35ae393451b6ceb1fedd24437bc0
CodeSoftly <eclittlejohn@yahoo.com>
2018-09-04 20:58:28 -0500

    Improved javascript and cleaning up
    I worked on the HTML and javascript by:
    
    - adding comments
    
    - improving id's in the HTML for cleaner code
    
    - improving the variable names to clean up the code
    
    - making each image correspond to the appropriate activity
    
    - appending the activity as a list item for each click

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
+// Declaring the variables to hold the id's and the activities to be posted at items.
 
-const myList = document.querySelector("ul");
 const images = ["#art", "#zoo", "#reading", "#swimming"];
-const thisList = ["arts and crafts", "go to the zoo", "reading", "swimming"];
+const activities = ["arts and crafts", "go to the zoo", "reading", "swimming"];
 
 
+// Looping through the images variable
+// Adding a click event on each image in the array
 for (let i = 0; i < images.length; i++) {
   document.querySelector(images[i]).addEventListener("click", () => {
-      myList.innerHTML = `<li>${thisList[i]}</li>`;
+ // Grabbing the unordered list and creating an empty list item
+      let ul = document.getElementById('list');
+      let li = document.createElement('li');
+ // Filling the empty list item and appending it to the unordered list
+      li.textContent = `${activities[i]}`;
+      ul.appendChild(li);
     }
   );
 }
